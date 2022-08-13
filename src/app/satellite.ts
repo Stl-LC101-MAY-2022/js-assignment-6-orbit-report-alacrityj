1e1e43b85941fc3feb0f24ccb1c7bae71395f0f7
Jennifer Krieger <alacritydanger@gmail.com>
2022-08-13 18:52:29 -0500

    fix isSpaceDebris

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -15,8 +15,10 @@ export class Satellite {
    }
 	
 	isSpaceDebris(): boolean {
-		if (this.type === "Space Debris")
-		return true;
+		if (this.type === "Space Debris") {
+			return true;
+		}
+			return false;
    }
 
 }
